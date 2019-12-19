ee4a7656d011913c0f40bdf9e6ac44e6bfcb6791
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-12-19 16:52:07 -0300

    save user on firebase realtime database

--- app/src/main/java/com/example/uber/activity/CadastroActivity.java
+++ app/src/main/java/com/example/uber/activity/CadastroActivity.java
@@ -4,13 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.uber.R;
 import com.example.uber.config.ConfiguracaoFirebase;
+import com.example.uber.helper.UsuarioFirebase;
 import com.example.uber.model.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
@@ -74,7 +79,7 @@ public class CadastroActivity extends AppCompatActivity {
     return switchTipoUsuario.isChecked() ? "M" : "P";
   }
 
-  public void cadastrarUsuario(Usuario usuario) {
+  public void cadastrarUsuario(final Usuario usuario) {
     autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
     autenticacao.createUserWithEmailAndPassword(
             usuario.getEmail(),
@@ -83,9 +88,52 @@ public class CadastroActivity extends AppCompatActivity {
       @Override
       public void onComplete(@NonNull Task<AuthResult> task) {
         if (task.isSuccessful()) {
+          try {
+            String idUsuario = task.getResult().getUser().getUid();
+            usuario.setId(idUsuario);
+            usuario.salvar();
+
+            //Atualizar nome no UserProfile
+            UsuarioFirebase.atualizarNomeUsuario(usuario.getNome());
+
+            // Redireciona o usuário com base no seu tipo
+            // Se o usuário for passageiro chama a activity maps
+            // Se for motorista a activity requisições
+            if (verificaTipoUsuario() == "P") {
+              startActivity(new Intent(CadastroActivity.this, MapsActivity.class));
+              finish();
+
+              Toast.makeText(CadastroActivity.this,
+                      "Sucesso ao cadastrar o passageiro!",
+                      Toast.LENGTH_SHORT).show();
+            } else {
+              startActivity(new Intent(CadastroActivity.this, RequisicoesActivity.class));
+              finish();
+
+              Toast.makeText(CadastroActivity.this,
+                      "Sucesso ao cadastrar o motorista!",
+                      Toast.LENGTH_SHORT).show();
+            }
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        } else {
+          String excecao = "";
+          try {
+            throw task.getException();
+          } catch (FirebaseAuthWeakPasswordException e) {
+            excecao = "Digite uma senha mais forte!";
+          } catch (FirebaseAuthInvalidCredentialsException e) {
+            excecao = "Por favor, digite um e-mail válido!";
+          } catch (FirebaseAuthUserCollisionException e) {
+            excecao = "Esta conta já foi cadastrada!";
+          } catch (Exception e) {
+            excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+            e.printStackTrace();
+          }
+
           Toast.makeText(CadastroActivity.this,
-                  "Sucesso ao cadastrar o usuário!",
-                  Toast.LENGTH_SHORT).show();
+                  excecao, Toast.LENGTH_SHORT).show();
         }
       }
     });
